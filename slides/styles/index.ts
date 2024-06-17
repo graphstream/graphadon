a2a1ed7c686376cce5e42a3a918182802237aeb3
Yoann Pigné <yoann.pigne@univ-lehavre.fr>
2024-06-17 16:07:04 +0200

    test1

---
+++ slides/styles/index.ts
@@ -0,0 +1,6 @@
+// inherit from base layouts, remove it to get full customizations
+import '@slidev/client/styles/layouts-base.css'
+// import './layout.css'
+import './override.css'
+import './code.css'
+import './custom.css'
