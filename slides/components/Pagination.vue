f6de66fed9713468560842baaa0c34df47bfa488
Yoann Pigné <yoann.pigne@univ-lehavre.fr>
2024-06-22 22:05:06 +0200

    pagination

---
+++ slides/components/Pagination.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="absolute p-2 text-xs c-gray-300" :class="classNames"><SlideCurrentNo /></div>
+  </template>
+  
+  <script setup lang="ts">
+  import { computed, PropType } from 'vue';
+  
+  const {
+    classNames: classNamesTransferred,
+    x,
+    y,
+  } = defineProps({
+    classNames: {
+      type: [Array, String] as PropType<string[] | string>,
+    },
+    x: {
+      default: 'r',
+      type: String as PropType<'l' | 'r'>,
+      validator: (value) => value === 'l' || value === 'r',
+    },
+    y: {
+      default: 't',
+      type: String as PropType<'b' | 't'>,
+      validator: (value) => value === 'b' || value === 't',
+    },
+  });
+  
+  const classNames = computed(() => [
+    ...(classNamesTransferred
+      ? Array.isArray(classNamesTransferred)
+        ? classNamesTransferred
+        : [classNamesTransferred]
+      : []),
+    x === 'l' && 'left-0',
+    x === 'r' && 'right-0',
+    y === 't' && 'top-0',
+    y === 'b' && 'bottom-0',
+  ]);
+  </script>
---
+++ slides/global-top.vue
@@ -0,0 +1,11 @@
+<template>
+  <Pagination
+    v-if="
+      $slidev.nav.currentPage !== $slidev.nav.total + 1 &&
+      !$slidev.themeConfigs.paginationPagesDisabled?.includes($slidev.nav.currentPage) &&
+      ($slidev.themeConfigs.paginationX || $slidev.themeConfigs.paginationY)
+    "
+    :x="$slidev.themeConfigs.paginationX"
+    :y="$slidev.themeConfigs.paginationY"
+  />
+</template>
